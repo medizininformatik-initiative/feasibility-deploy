0ed1d099f67713ab7f6856e6a01b6ee5f52ff3b6
Julian Gründner <juliangruendner@googlemail.com>
2021-12-15 09:39:56 +0100

    update flare and feasibility backend

--- zars/start-zars.sh
+++ zars/start-zars.sh
@@ -5,7 +5,6 @@ COMPOSE_PROJECT=codex-deploy
 BASE_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 docker-compose -p $COMPOSE_PROJECT -f $BASE_DIR/keycloak/docker-compose.yml up -d
-docker-compose -p $COMPOSE_PROJECT -f $BASE_DIR/flare/docker-compose.yml up -d
 docker-compose -p $COMPOSE_PROJECT -f $BASE_DIR/backend/docker-compose.yml up -d
 docker-compose -p $COMPOSE_PROJECT -f $BASE_DIR/gui/docker-compose.yml up -d
 docker-compose -p $COMPOSE_PROJECT -f $BASE_DIR/aktin-broker/docker-compose.yml up -d
