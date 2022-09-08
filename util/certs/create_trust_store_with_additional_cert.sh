bff8500333331d48db8bcb7701e0274b1807bed1
Julian Gründner <juliangruendner@googlemail.com>
2022-09-08 17:39:07 +0200

    change to lint

--- util/certs/create_trust_store_with_additional_cert.sh
+++ util/certs/create_trust_store_with_additional_cert.sh
@@ -2,7 +2,7 @@
 
 BASE_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-docker build -t feasibility-deploy-create-trust-store ${BASE_DIR}/
+docker build -t feasibility-deploy-create-trust-store "${BASE_DIR}"/
 docker run  --name create-trust-store --rm \
     -v "${BASE_DIR}/your_certs":/opt/certs/your_certs \
     -v "${BASE_DIR}/trust_store_output":/opt/certs/output \
