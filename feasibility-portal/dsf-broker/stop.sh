9e377eed671af3115800195b2eaff89462113920
Julian Gründner <juliangruendner@googlemail.com>
2023-03-29 17:42:06 +0200

    remove dsf from deployment

--- feasibility-portal/dsf-broker/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-# Arguments
-#   1: docker-compose project token
-
-BASE_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P )"
-
-docker-compose -p "$1" -f "$BASE_DIR"/docker-compose.yml stop
