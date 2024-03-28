4e310db1424e996d02d31f00c62b8fa507a94abf
Mathias Rühle <math2306@hotmail.com>
2024-03-28 08:59:48 +0100

    Make Certificate Firefox Compatible
    For the firefox browser to accept the server certificate, it needs not
    to be a self-signed ca, but a non-ca certificate which is signed by a
    self-signed ca.
    Add environment variables to make certificates configurable.

--- feasibility-portal/setup-base-auth.sh
+++ feasibility-portal/setup-base-auth.sh
@@ -11,8 +11,22 @@
 BASE_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P )"
 
 echo "Generating default certificate..."
-docker run --rm -v "$BASE_DIR"/auth:/export --entrypoint openssl alpine/openssl req -nodes -subj '/CN=localhost' -addext "subjectAltName = DNS:datenportal.localhost, DNS:auth.datenportal.localhost, DNS:api.datenportal.localhost" -x509 -newkey rsa:4096 -keyout /export/key.pem -out /export/cert.pem -days 99999
-docker run --rm -v "$BASE_DIR"/auth:/export alpine chmod -R 655 /export
+docker run --rm -u "$(id -u):$(id -g)" -v "$BASE_DIR"/auth:/export alpine/openssl req -nodes -subj '/CN=local-ca' -x509 -newkey rsa:4096 -keyout /export/ca-key.pem -out /export/ca-cert.pem -days 99999
+docker run --rm -u "$(id -u):$(id -g)" -v "$BASE_DIR"/auth:/export alpine/openssl req -nodes -subj '/CN=localhost' -x509 -CA /export/ca-cert.pem -CAkey /export/ca-key.pem -addext "basicConstraints=CA:false" -addext "subjectAltName = DNS:datenportal.localhost, DNS:auth.datenportal.localhost, DNS:api.datenportal.localhost" -newkey rsa:4096 -keyout /export/key.pem -out /export/cert.pem -days 99998
+docker run --rm -u "$(id -u):$(id -g)" -v "$BASE_DIR"/auth:/export alpine chmod -R 655 /export/cert.pem /export/key.pem
+if [ -f "$BASE_DIR"/auth/ca-cert.pem ] && [ -f "$BASE_DIR"/auth/cert.pem ]
+then
+  echo "Certificate has been generated."
+  echo
+  echo "  Import the ca certificate '$BASE_DIR/auth/ca-cert.pem' in your browser's certificate store and trust it with identifying websites."
+  echo "  Add the following line to your hosts file (e.g. '/etc/hosts'):"
+  echo
+  echo "    127.0.0.1  datenport.localhost auth.datenportal.localhost api.datenportal.localhost"
+  echo
+  echo "  If the portal will not be running on your local computer replace '127.0.0.1' with the actual ip address where the portal will be running."
+  echo
+fi
+
 
 #echo "generating user: $1 , with password: $2"
 #docker run --rm --entrypoint htpasswd registry:2.7.0 -nb $1 $2 > .htpasswd
