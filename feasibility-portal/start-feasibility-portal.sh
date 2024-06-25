b32c8c2e147bf3432c57832c808e77a40fcd9220
Julian Gründner <juliangruendner@googlemail.com>
2024-06-25 16:55:09 +0200

    bump ui version and update docker compose calls

--- feasibility-portal/start-feasibility-portal.sh
+++ feasibility-portal/start-feasibility-portal.sh
@@ -4,8 +4,8 @@ COMPOSE_PROJECT=${FEASIBILITY_COMPOSE_PROJECT:-feasibility-deploy}
 
 BASE_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P )"
 
-docker-compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/keycloak/docker-compose.yml up -d
-docker-compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/backend/docker-compose.yml up -d
-docker-compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/gui/docker-compose.yml up -d
-docker-compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/proxy/docker-compose.yml up -d
+docker compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/keycloak/docker-compose.yml up -d
+docker compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/backend/docker-compose.yml up -d
+docker compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/gui/docker-compose.yml up -d
+docker compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/proxy/docker-compose.yml up -d
 
--- feasibility-triangle/reset-fhir-server.sh
+++ feasibility-triangle/reset-fhir-server.sh
@@ -3,5 +3,5 @@
 COMPOSE_PROJECT=${COMPOSE_PROJECT:-feasibility-deploy}
 
 BASE_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P )"
-COMPOSE_IGNORE_ORPHANS=True docker-compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/fhir-server/docker-compose.yml down -v
-COMPOSE_IGNORE_ORPHANS=True docker-compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/fhir-server/docker-compose.yml up -d
+COMPOSE_IGNORE_ORPHANS=True docker compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/fhir-server/docker-compose.yml down -v
+COMPOSE_IGNORE_ORPHANS=True docker compose -p "$COMPOSE_PROJECT" -f "$BASE_DIR"/fhir-server/docker-compose.yml up -d
