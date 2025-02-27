c328848a403bd271b3ded2c8b6276e37623e83e0
Mathias Rühle <math2306@hotmail.com>
2025-02-27 14:58:15 +0100

    Add Missing Base Directory to .env File Initialization Script

--- feasibility-portal/initialise-portal-env-files.sh
+++ feasibility-portal/initialise-portal-env-files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-envfiles=( "gui/deploy-config.json" "backend/.env" "keycloak/.env" "proxy/.env")
+BASE_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P )"
+envfiles=( "$BASE_DIR/gui/deploy-config.json" "$BASE_DIR/backend/.env" "$BASE_DIR/keycloak/.env" "$BASE_DIR/proxy/.env")
 
 for file in "${envfiles[@]}"
 do
